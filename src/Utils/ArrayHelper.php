cabfacaff88d77cd75310783c2c82677d0a63514
Scott Drinkwater <s.p.drinkwater@gmail.com>
2024-03-11 14:13:45 +0000

    Array helper to see if string exists using multiple needs

---
+++ src/Utils/ArrayHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types= 1);
+
+namespace App\Utils;
+
+class ArrayHelper {
+    public static function stripos(string $haystack, array $needles, int $offset = 0): bool {
+        foreach($needles as $needle) {
+            if(stripos($haystack, $needle, $offset) !== false) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+}
